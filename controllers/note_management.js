f91e313fea9d346eb94cb3974551e0fe95762c29
unknown <huyhoang250620@gmail.com>
2023-04-09 22:30:58 +0700

    HoangNH

--- controllers/note_management.js
+++ controllers/note_management.js
@@ -37,20 +37,39 @@ exports.createNote = async (req, res) => {
 //get dữ liệu
 exports.getNote = async (req, res) => {
   const departRef = db.collection(table);
-  try{
-    departRef.get().then((snapshot) => {
-          const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-      }));
-          console.log(data);
-          return res.status(201).json(data);
-      })
-  } catch (error) {
-      return res
-      .status(500)
-      .json({ general: "Something went wrong, please try again"});          
+  if(req.query.MaGV == undefined){
+    try{
+      departRef.get().then((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+            console.log(data);
+            return res.status(201).json(data);
+        })
+    } catch (error) {
+        return res
+        .status(500)
+        .json({ general: "Something went wrong, please try again"});          
+    }
   }
+  else{
+    try{
+      departRef.where('MaGV','==',req.query.MaGV).get().then((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+            console.log(data);
+            return res.status(201).json(data);
+        })
+    } catch (error) {
+        return res
+        .status(500)
+        .json({ general: "Something went wrong, please try again"});          
+    }
+  }
+  
 };
 //update
 exports.updateNote = async (req, res) => {
